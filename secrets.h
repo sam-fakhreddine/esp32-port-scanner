1a34dab563f8f5eddf4fb92ef948cff416746762
Sam Fakhreddine <sam.fakhreddine@rackspace.com>
2025-10-17 08:32:21 -0600

    Reorganize presentation: home network security narrative with ESP32 reveal

---
+++ secrets.h
@@ -0,0 +1,12 @@
+#ifndef SECRETS_H
+#define SECRETS_H
+
+// WiFi Credentials
+#define WIFI_SSID "sambou"
+#define WIFI_PASSWORD "7809960763"
+
+// MQTT Configuration
+#define MQTT_SERVER "192.168.0.210"
+#define MQTT_PORT 12883
+
+#endif
